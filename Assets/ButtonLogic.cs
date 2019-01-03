5fb1d4c7dd44efb47803f018b7f90c21988ad9ad
Simone S <xsimone97@hotmail.it>
2019-01-03 15:34:09 +0100

    Proj upload

---
+++ Assets/ButtonLogic.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonLogic : MonoBehaviour
+{
+    //touch position in world coordinates
+    private Vector2 touch_pos_world;
+    //layer mask of the buttons (the only affected by the raycast)
+    public LayerMask layer_mask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check if there has been a touch
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            //transform the touch to world coordinates
+            touch_pos_world = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+
+            //We now raycast with this information. If we have hit something we can process it.
+            RaycastHit2D hitInformation = Physics2D.Raycast(touch_pos_world, Camera.main.transform.forward, Mathf.Infinity,layer_mask);
+
+            if (hitInformation.collider != null)
+            {
+                //deactivate the g.o.
+                hitInformation.transform.gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //reference
+    public GameManager GM;
+    //number of rows & cloumns
+    static int rowcol = 4;
+    //button position
+    private Vector2[,] position = new Vector2[rowcol, rowcol];
+    //button instances
+    private GameObject[,] buttons = new GameObject[rowcol, rowcol];
+    //button prefab
+    public GameObject button;
+    //button father
+    public Transform button_father;
+
+    // Before the start
+    void Awake()
+    {
+        //ref assignment
+        GM = this;
+        //variables def
+        for (int i = 0; i < rowcol; i++)
+        {
+            for (int j = 0; j < rowcol; j++)
+            {
+                //assign the position of the points in the scene
+                position[i, j] = Camera.main.ScreenToWorldPoint(new Vector3(
+                    (Screen.width / rowcol) * i + (Screen.width / rowcol) / 2,
+                    (Screen.height / (rowcol+2)) * j + (Screen.height / (rowcol+2)) / 2)); //some space for the scoreboard
+                //create the g.o. for each point
+                buttons[i,j] = Instantiate(button, new Vector3(position[i, j].x, position[i, j].y, 0), Quaternion.identity, button_father);
+                buttons[i, j].SetActive(false);
+            }
+        }
+    }
+}
